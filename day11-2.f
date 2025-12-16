53a000dc677b92770803849693077500267e0190
Tim Holyoake <timholyoake@btopenworld.com>
2025-12-16 19:32:05 +0000

    Day 11 part 2 solution

---
+++ day11-2.f
@@ -0,0 +1,210 @@
+      PROGRAM DAY11P2
+C
+C     Assume graph is no larger than 700 nodes, each with no
+C     more than 25 leaves - parameterised below.
+C
+      INTEGER MAXNODES,MAXLEAVES,DACFFTSTATES
+      PARAMETER (MAXNODES=700)
+      PARAMETER (MAXLEAVES=25)
+C     State 1 = neither seen on path, State 2 = only FFT seen
+C     State 3 = only DAC seen, State 4 = DAC and FFT seen
+      PARAMETER (DACFFTSTATES=4)
+C
+      CHARACTER*256 PUZZLE
+      CHARACTER*3 NODE,LEAVES(MAXLEAVES)
+      COMPLEX GRAPH(MAXNODES,MAXLEAVES+1),STR2CMPLX
+      INTEGER L1,NL,NC,SOURCE,DEST,FFT,DAC,VFFTDAC
+      INTEGER*8 TOTAL,DFSMEMO
+      INTEGER*8 MEMO(MAXNODES,DACFFTSTATES)
+C
+   10 FORMAT(A)
+   20 FORMAT(A)
+   30 FORMAT(A,I16)
+C
+      WRITE(*,10)"Advent of Code 2025 day 11, part 2"
+      WRITE(*,10)" "
+C
+      OPEN(10,FILE="day11in.txt",STATUS="OLD",FORM="FORMATTED",
+     +     ACCESS="SEQUENTIAL",ACTION="READ")
+      NC=0
+   50 CONTINUE
+      READ(10,FMT=20,ERR=999,END=100) PUZZLE
+C     Count the nodes read in
+      NC=NC+1
+      CALL SPLIT(PUZZLE,NODE,LEAVES,NL)
+C     First element of each row of the graph is a complex number,
+C     The real element represent the value of the node's name,
+C     the imaginary element is set to the number of leaves that
+C     the node has (currently 0) by STR2CMPLX
+      GRAPH(NC,1)=STR2CMPLX(NODE,0)
+C     Record the NC index of the SOURCE node "svr" = 192218
+      IF (INT(REAL(GRAPH(NC,1))).EQ.192218) SOURCE=NC
+C     Record the NC indices for FFT (60620) and DAC (40103)
+      IF (INT(REAL(GRAPH(NC,1))).EQ.60620) FFT=NC
+      IF (INT(REAL(GRAPH(NC,1))).EQ.40103) DAC=NC
+C     Remaining elements are the leaves. Leaf name value in the
+C     real part of the complex number, imaginary part is 0 (may
+C     be useful for solving part 2 perhaps?)
+      DO L1=1,NL
+        GRAPH(NC,L1+1)=STR2CMPLX(LEAVES(L1),0)
+      ENDDO
+C     Update the leaf count on the NODE element NC,1
+      GRAPH(NC,1)=GRAPH(NC,1)+CMPLX(0,NL)
+C
+C     WRITE(*,*) NC,(GRAPH(NC,L1),L1=1,NL+1)
+      GOTO 50
+  100 CONTINUE
+      CLOSE(10)
+C     Add a node for "out" (DEST) with no leaves
+      NC=NC+1
+      GRAPH(NC,1)=STR2CMPLX("out",0)
+      DEST=NC
+C
+C     WRITE(*,*) NC,(GRAPH(NC,L1),L1=1,NL+1)
+C     Set up the memo array - use -1 to flag not recoreded.
+C     This will record the number of paths (by state) to 
+C     this node.
+      DO L1=1,MAXNODES
+        DO L2=1,DACFFTSTATES
+          MEMO(L1,L2)=-1
+        ENDDO
+      ENDDO
+C     Not visited DAC or FFT - set this to 1
+      VFFTDAC=1
+C
+C     Call the Memoized DFS graph walking function to find the
+C     number of distinct paths from SOURCE to DEST via FFT and DAC
+C
+      TOTAL=DFSMEMO(GRAPH,SOURCE,DEST,VFFTDAC,FFT,DAC,MEMO)
+C
+      WRITE(*,30)"Number of paths from svr to out is ",TOTAL
+  999 CONTINUE
+      END
+C
+      SUBROUTINE SPLIT(PUZZLE,NODE,LEAVES,NL)
+C
+C     Splits the string PUZZLE, format xxx: yyy .. zzz
+C     into NODE, LEAVES(NL). Contents of PUZZLE destroyed!
+C     Assumes every NODE, LEAVES string is exactly 3 characters long
+C
+      CHARACTER*256 PUZZLE
+      CHARACTER*3 NODE,LEAVES(*)
+      INTEGER NL
+C
+C     Fixed format - NODE is the first 3 characters of PUZZLE
+      NODE=PUZZLE(1:3)
+C     Fixed format - skip to the first leaf past : and <space>
+      PUZZLE=PUZZLE(6:)
+      NL=0
+      DO WHILE (PUZZLE(1:1).NE." ")
+        NL=NL+1
+        LEAVES(NL)=PUZZLE(1:3)
+        PUZZLE=PUZZLE(5:)
+      ENDDO
+C  
+      RETURN
+      END
+C
+      COMPLEX FUNCTION STR2CMPLX(NODE,VAL)
+      CHARACTER*3 NODE
+      INTEGER VAL
+C
+      INTEGER L1,NODEVAL
+C
+      NODEVAL=0
+      DO L1=1,3
+C       Assumes all node names are lower case! Converts a to 1,
+C       b to 2, etc. Multiplies each by 100. So aaa = 10101, 
+C       aab = 10102 ... zzz = 262626
+        NODEVAL=NODEVAL*100+(ICHAR(NODE(L1:L1))-(ICHAR("a")-1))
+      ENDDO
+C     Convert to complex number for return
+      STR2CMPLX=CMPLX(NODEVAL,0)
+      RETURN
+      END
+C
+      RECURSIVE INTEGER*8 FUNCTION 
+     +   DFSMEMO(GRAPH,THISNODE,DEST,VFFTDAC,FFT,DAC,MEMO) 
+     +   RESULT(RESULT)
+C
+      COMPLEX GRAPH(700,26)
+      INTEGER THISNODE,DEST,FFT,DAC,VFFTDAC,NEWFFTDAC
+      INTEGER*8 MEMO(700,4)
+C     
+      INTEGER L1,NEIGHBOUR
+      COMPLEX NEIGHLEX
+      LOGICAL FOUND,THRUFFT,THRUDAC
+C
+C     WRITE(*,*)"This node is ",THISNODE,GRAPH(THISNODE,1)
+C     WRITE(*,*)"Destination is ",DEST,GRAPH(DEST,1)
+C
+C     Have we cached the result? If so, return it.
+C
+      IF (MEMO(THISNODE,VFFTDAC).GT.-1) THEN
+C       WRITE(*,*)"Retrieving cache at ",THISNODE,VFFTDAC
+        RESULT=MEMO(THISNODE,VFFTDAC)
+C       WRITE(*,*)"Cache is ",RESULT
+        RETURN
+      ENDIF
+C
+C     Are we at "out"? If so, return RESULT=1 if VFFTDAC=4,
+C     otherwise 0.
+C
+      IF (THISNODE.EQ.DEST) THEN
+        IF (VFFTDAC.EQ.4) THEN
+          RESULT=1
+        ELSE
+          RESULT=0
+        ENDIF
+C       WRITE(*,*)"At out ", RESULT
+        RETURN
+      ENDIF
+C
+C     Create NEWFFTDAC appropriately
+C
+C     WRITE(*,*)"VFFTDAC was ",VFFTDAC
+      THRUDAC=.FALSE.
+      THRUFFT=.FALSE.
+      IF ((VFFTDAC.EQ.4).OR.(VFFTDAC.EQ.3)) THRUDAC=.TRUE. 
+      IF ((VFFTDAC.EQ.4).OR.(VFFTDAC.EQ.2)) THRUFFT=.TRUE.
+      IF (THISNODE.EQ.DAC) THRUDAC=.TRUE.
+      IF (THISNODE.EQ.FFT) THRUFFT=.TRUE.
+C
+      NEWFFTDAC=1
+      IF ((THRUDAC).AND.(THRUFFT)) NEWFFTDAC=4
+      IF ((THRUDAC).AND.(.NOT.THRUFFT)) NEWFFTDAC=3
+      IF ((.NOT.THRUDAC).AND.(THRUFFT)) NEWFFTDAC=2
+C 
+C     WRITE(*,*)"NEWFFTDAC now ",NEWFFTDAC
+C
+C     Recursively call this function
+C
+      RESULT=0
+      DO L1=1,INT(IMAG(GRAPH(THISNODE,1)))
+        NEIGHLEX=GRAPH(THISNODE,L1+1)
+C       Find the array index of this neighbour
+        FOUND=.FALSE.
+        NEIGHBOUR=1
+        DO WHILE (.NOT.FOUND)
+          IF (INT(REAL(NEIGHLEX)).EQ.
+     +        INT(REAL(GRAPH(NEIGHBOUR,1)))) THEN
+            FOUND=.TRUE.
+          ELSE
+            NEIGHBOUR=NEIGHBOUR+1
+          ENDIF
+        ENDDO
+C       WRITE(*,*)"Neighbour index is ",NEIGHBOUR
+        RESULT=RESULT+DFSMEMO(GRAPH,NEIGHBOUR,DEST,NEWFFTDAC,
+     +                        FFT,DAC,MEMO)
+C       WRITE(*,*)"After recursion ",RESULT
+      ENDDO
+C
+C     Update the MEMO cache ...
+
+C     WRITE(*,*)"MEMO updated ",THISNODE,VFFTDAC," with ",RESULT 
+      MEMO(THISNODE,VFFTDAC)=RESULT
+C
+C     ... and return RESULT
+C
+      RETURN
+      END
